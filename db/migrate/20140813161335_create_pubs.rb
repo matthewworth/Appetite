dfce10ebe60ddb5104d5658425dd905a313ec09d
Matt Worth <matt.worth@gmail.com>
2014-08-13 17:39:41 +0100

    4th commit

--- db/migrate/20140813161335_create_pubs.rb
+++ db/migrate/20140813161335_create_pubs.rb
@@ -1,6 +1,10 @@
 class CreatePubs < ActiveRecord::Migration
   def change
     create_table :pubs do |t|
+    t.string :name
+    t.string :address
+    t.lattitude :string
+    t.longitude :string
 
       t.timestamps
     end
